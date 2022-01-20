ddbb85d1f1f55206ede6587fdc41dd0ef31b6d08
Callum Duffield <duffieldcallum123@gmail.com>
2022-01-20 17:30:41 +0000

    I have updated the paint calculator features

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,75 @@ public class Main {
 
     public static void main(String[] args) {
         String PaintName = "Duffys Magic Paint";
-        int amountOfWalls;
-        amountOfWalls = (int) (Math.random() * 10);
         double priceOfPaintPerMeter = 6.24;
         int paintCoveragePerMeter = 10;
 
-        int wallHeight = 7;
-        int wallWidth = 7;
-        int wallTotalArea = wallHeight * wallWidth * amountOfWalls;
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter the amount of walls you need painting?");
+        int amountOfWalls = Integer.parseInt(userInput.nextLine());
+        System.out.println("Please enter the height of your walls in meters?");
+        int wallsHeight = Integer.parseInt(userInput.nextLine());
+        System.out.println("Please enter the width of your walls in meters?");
+        int wallsWidth = Integer.parseInt(userInput.nextLine());
 
-        int priceOfPainterPerHour = 30;
-        int howManyHoursWorking = amountOfWalls;
-        int totalLabourCost = priceOfPainterPerHour * howManyHoursWorking;
+        int wallTotalArea = (wallsWidth * wallsHeight) * amountOfWalls;
+
+        System.out.println("How much do you charge per worker per hour of labour?");
+        int pricePerHour = Integer.parseInt(userInput.nextLine());
+        System.out.println("Roughly how many hours will this work take?");
+        int howManyHoursWork = Integer.parseInt(userInput.nextLine());
+        int howManyWorkers = amountOfWalls;
+        System.out.println("What is the customers budget in pounds?");
+        int customersBudget = Integer.parseInt(userInput.nextLine());
 
 
-        int paintCansNeeded = (int) Math.ceil (wallTotalArea / paintCoveragePerMeter);
 
-        double totalPricePaint = paintCansNeeded * priceOfPaintPerMeter;
+
+        int totalLabourCost = (pricePerHour * howManyHoursWork) * howManyWorkers;
+
+        String laddersNeededOrNot = (wallsHeight >= 10) ? "Needs Ladder" : "Doesn't need Ladder";
+
+        int paintCansNeeded = (int) Math.ceil(wallTotalArea / paintCoveragePerMeter);
+        double totalPricePaint = (double) (paintCansNeeded * priceOfPaintPerMeter);
         double totalCost = totalPricePaint + totalLabourCost;
+        if (customersBudget > totalCost) {
+            System.out.println ("The customers budget is in range");
+        } else if (customersBudget == totalCost) {
+
+            System.out.println ("The customers budget is in range");
+        }
+
+        else {
+            System.out.println("The quote is too expensive for the customer");
+        }
+
+        String message;
+        switch (howManyHoursWork) {
+            case 0:
+                message = "No hours required to work";
+                break;
+            case 1:
+                message = "One hours work is required";
+                break;
+            case 2:
+                message = "Two hours work is required";
+                break;
+            default:
+                message = "Over two hours work is required";
+                break;
+        }
 
 
-        System.out.println("The total cost for " + PaintName + " dircdof this quote is " + totalPricePaint + " pounds");
+        System.out.println("The total cost for " + PaintName + " will be " + totalPricePaint + " pounds");
         System.out.println("The total number of paint cans needed for this job is " + paintCansNeeded);
         System.out.println("The total number of walls we will be painting is " + amountOfWalls);
         System.out.println("The total area of the walls needed painting in meters is " + wallTotalArea);
         System.out.println("The total cost of labour hours is " + totalLabourCost + " pounds");
         System.out.println("The total cost for this job is " + totalCost + " pounds");
+        System.out.println(laddersNeededOrNot);
+        System.out.println(message);
     }
-    }
+
+}
+
+
